a8b9415d47c5e771438fe058ce1a6b94b8564d10
Manan Dhiman <64782929+MananDhiman@users.noreply.github.com>
2022-07-07 18:39:26 +0530

    Create 1.cpp

---
+++ cpp/1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float sub_1, sub_2, sub_3, sub_4, sub_5, sub_6, total;
+
+  cout << "Enter Marks of First Subject: \n";
+  cin >> sub_l;
+
+  cout << "Enter Marks of Second Subject: \n";
+  cin >> sub_2:
+
+  cout << “Enter Marks of Third Subject: \n";
+  cin >> sub_3;
+
+  cout << "Enter Marks of Fourth Subject: \n";
+  cin >> sub_4;
+
+  cout << “Enter Marks of Fifth Subject: \n";
+  cin >> sub_5;
+
+  cout << "Enter Marks of Sixth Subject: \n";
+  cin >> sub_6;
+
+  total = sub_1 + sub 2 + sub_3 + sub_4 + sub_5 + sub_6;
+  cout << "The Total Marks Scored Are: " << total;
+
+  return 0;
+}
