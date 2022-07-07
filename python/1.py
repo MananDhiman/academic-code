b7a89aaf541845d4e6ac7d00996f1c4a5d6f28ef
Manan Dhiman <64782929+MananDhiman@users.noreply.github.com>
2022-07-07 18:57:58 +0530

    Create 1.py
    Compute sum, subtraction, multiplication, division and exponent of given variables input by the user.

---
+++ python/1.py
@@ -0,0 +1,8 @@
+num_1: float = float(input("Enter First Number:\n"))
+num_2: float = float(input("Enter Second Number:\n"))
+
+print("The Sum is: " + str(num_1+num_2))
+print("The Difference is: " + str(num_1-num_2))
+print("The Product is: " + str(num_1*num_2))
+print("The Quotient is: " + str(num_1/num_2))
+print("The Exponent is: " + str(pow(num_1, num_2)))
